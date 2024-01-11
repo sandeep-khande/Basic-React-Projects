8008530c61c18ac984545b5ba408796804b00e03
sandeep-khande <sandeepkhande448@gmail.com>
2024-01-11 23:49:27 +0530

    completed

--- 02bgChanger/src/App.jsx
+++ 02bgChanger/src/App.jsx
@@ -1,15 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("olive")
 
   return (
-    <>
-      
-    </>
+    <div className='w-full h-screen duration-200' style={{backgroundColor: color}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap gap-3 shadow-lg bg-white justify-center px-3 py-2 rounded-3xl'>
+          <button onClick={() => setColor("red")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "red"}}>Red</button>
+          <button onClick={() => setColor("blue")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "blue"}}>blue</button>
+          <button onClick={() => setColor("green")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "green"}}>green</button>
+          <button onClick={() => setColor("violet")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "violet"}}>violet</button>
+          <button onClick={() => setColor("yellow")} className='outline-none px-4 py-1 rounded-full text-black shadow-lg' style={{backgroundColor: "yellow"}}>yellow</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
